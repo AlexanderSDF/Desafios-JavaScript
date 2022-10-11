bdeccb546017d39a56da01da3fa9107344047776
Alexander <alexanderj146@gmail.com>
2022-10-11 12:14:04 -0300

    Creando PreEntrega1

---
+++ index.js
@@ -0,0 +1,17 @@
+//!SIMULADOR DE COMPRA
+//! Muestra al cliente los productos disponibles.
+//! Consulta al cliente cuantos productos va solicitar del efecto indicado 
+//!En base a la cantidad disponible en stock devuelve al cliente un msj.
+
+//!VARIABLES----------------------------------------------------------------
+let producto1 = 30
+
+let producto2 = 20
+
+let producto3 = 50
+//!CONSTANTES Y FUNCIONES---------------------------------------------------
+
+function suma() {
+    num1 + num2 = resultado; 
+}
+//!CÓDIGO-------------------------------------------------------------------

cab56a4a2ac351e6324444a88be8913c2af393a1
Alexander <alexanderj146@gmail.com>
2022-11-01 18:33:01 -0300

    changes

--- proyecto.js
+++ proyecto.js
@@ -1,7 +1,7 @@
 //!--------------------------PROYECTO TIENDA----------------------------------------------
 //Creo la clase Producto, con las propiedades id, nombre, precio y cantidad:
 class Producto {
-	constructor(id, marca, modelo, categoria, pantalla, color, precio, cantidad) {
+	constructor(id, marca, modelo, categoria, pantalla, color, precio, disponible) {
 		this.id = id;
 		this.marca = marca;
 		this.modelo = modelo;
@@ -10,19 +10,29 @@ class Producto {
 		this.color = color;
 		this.precio = precio;
 		this.disponible = disponible;
+		this.compra = 1;
 	}
 }
 //Creo productos y los almaceno en un array:
-const producto1 = new Producto(1, "Lenovo", "falta", "Office", "13'", "Plateado", 67000, 5);
-const producto2 = new Producto(2, "Msi", "falta", "Office", "14'", "Blanco", 72000, 5);
-const producto3 = new Producto(3, "Huawei", "falta", "Office", "15'", "Negro", 95000,5);
-const producto4 = new Producto(4, "Apple", "falta", "HardWork", "13'", "Plateado", 230000, 5);
-const producto5 = new Producto(5, "Asus", "falta", "HardWork", "14'", "Negro", 280000, 5);
-const producto6 = new Producto(6, "Alienware", "falta", "Gaming", "14'", "Blanco", 300000, 5);
-const producto7 = new Producto(7, "Lenovo", "falta", "Gaming", "15'", "Negro", 290000, 5);
-const producto8 = new Producto(8, "Asus", "falta", "Gaming", "15'", "Plateado", 315000, 5);
+const producto1 = new Producto(1, "Lenovo", "Thinkpad", "Office", "13'", "Plateado", 20000, 5);
+const producto2 = new Producto(2, "Msi", "Carbon", "Office", "14'", "Blanco", 30000, 5);
+const producto3 = new Producto(3, "Huawei", "MateBook", "Office", "15'", "Negro", 40000, 5);
+const producto4 = new Producto(4, "Apple", "Air M1", "HardWork", "13'", "Plateado", 80000, 5);
+const producto5 = new Producto(5, "Asus", "Lite", "HardWork", "14'", "Negro", 100000, 5);
+const producto6 = new Producto(6, "Alienware", "X15", "Gaming", "14'", "Blanco", 150000, 5);
+const producto7 = new Producto(7, "Msi", "Pro Carbon", "Gaming", "15'", "Negro", 290000, 5);
+const producto8 = new Producto(8, "Asus", "Rog Z96", "Gaming", "15'", "Plateado", 315000, 5);
 
-const productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8];
+const productos = [
+	producto1,
+	producto2,
+	producto3,
+	producto4,
+	producto5,
+	producto6,
+	producto7,
+	producto8,
+];
 
 //Muestro los productos modificando el DOM.
 const contenedorProductos = document.getElementById("contenedorProductos");
@@ -32,14 +42,16 @@ productos.forEach((producto) => {
 	divProducto.classList.add("card", "col-xl-3", "col-md-6", "col-sm-12");
 	divProducto.innerHTML = `
                             <div>
-                                <img src="../images/${producto.id}.jpg" class="card-img-top img-fluid py-3">
+                                <img src="./images/${producto.id}.jpg" class="card-img-top img-fluid py-3">
                                 <div class="card-body">
-                                    <h3 class="card-title"> ${producto.marca} </h3>
-                                    <p class="card-text"> $${producto.precio} </p>
+                                    <h3 class="card-title"> ${producto.marca} ${producto.modelo} ${producto.pantalla}</h3>
+                                    <p class="card-text"> $ ${producto.precio} </p>
+									<p class="card-text"> En Stock: ${producto.disponible} </p>
                                     <button id="botón${producto.id}" class="btn btn-primary"> Agregar al Carrito </button>
                                 </div>
                             </div>`;
 	contenedorProductos.appendChild(divProducto);
+
 	//Agregar un evento al botón de agregar al carrito:
 	const boton = document.getElementById(`botón${producto.id}`);
 	boton.addEventListener("click", () => {
@@ -55,12 +67,24 @@ const agregarAlCarrito = (id) => {
 	const producto = productos.find((producto) => producto.id === id);
 	const productoEnCarrito = carrito.find((producto) => producto.id === id);
 	if (productoEnCarrito) {
-		productoEnCarrito.cantidad++;
+		productoEnCarrito.compra++;
 	} else {
 		carrito.push(producto);
 	}
 	actualizarCarrito();
+	verificarDisponible();
 };
+
+
+//todo Verificar cantidad en sotck
+function verificarDisponible(producto) {
+	if (compra <= producto.disponible) {
+	console.log(`La cantidad de notebooks compradas es de: ${compra}`);
+	} else {
+		alert(`No hay en Stock la cantidad ingresada, vuelva a intentarlo.`);
+		actualizarCarrito();
+	}
+}
 
 //Muestro el carrito de compras modificando el DOM.
 
@@ -76,14 +100,13 @@ function actualizarCarrito() {
                 <div class="card col-xl-3 col-md-6 col-sm-12">
                     <img src="../images/${producto.id}.jpg" class="card-img-top img-fluid py-3">
                     <div class="card-body">
-                        <h3 class="card-title"> ${producto.nombre} </h3>
-                        <p class="card-text"> $${producto.precio} </p>
+                        <h3 class="card-title"> ${producto.marca} </h3>
+                        <p class="card-text"> $ ${producto.precio} </p>
                         <button onClick = "eliminarDelCarrito(${producto.id})" class="btn btn-primary"> Eliminar del Carrito </button>
                     </div>
                 </div>
                 `;
 	});
-
 	contenedorCarrito.innerHTML = aux;
 	calcularTotalCompra();
 }
@@ -96,7 +119,7 @@ const eliminarDelCarrito = (id) => {
 	actualizarCarrito();
 };
 
-///Función para vaciar todo el carrito por completo:
+//Función para vaciar todo el carrito
 
 const vaciarCarrito = document.getElementById("vaciarCarrito");
 vaciarCarrito.addEventListener("click", () => {
@@ -104,14 +127,14 @@ vaciarCarrito.addEventListener("click", () => {
 	actualizarCarrito();
 });
 
-//Creo una función que me calcule el total del carrito:
+//Función que calcula el total del carrito:
 
 const totalCompra = document.getElementById("totalCompra");
 
 const calcularTotalCompra = () => {
 	let total = 0;
 	carrito.forEach((producto) => {
-		total += producto.precio * producto.cantidad;
+		total += producto.precio * producto.compra;
 	});
 	totalCompra.innerHTML = total;
 };
